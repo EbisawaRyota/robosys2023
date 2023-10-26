1ce9ac65515b7770b10187c00a68784750a1d0df
Ryota Ebisawa <s22C1014KY@s.chibakoudai.jp>
2023-10-26 12:54:02 +0900

    add command

--- test.bash
+++ test.bash
@@ -1,19 +1,23 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-	echo NG at Line $1
-	res=1
-}
+$ ./test.bash
 
 res=0
++ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-
+++ seq 5
+++ ./plus
++ out=15
 [ "${out}" = 15 ] || ng ${LINENO}
++ '[' 15 = 15 ']'
 
 [ "$res" = 0 ] && echo OK
++ '[' 0 = 0 ']'
++ echo OK
+OK
 exit $res
-
++ exit 0
