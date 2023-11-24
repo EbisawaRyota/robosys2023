9e8febc6e3ccaf777a13b47533e97c4c7368eb47
Ryota Ebisawa <s22C1014KY@s.chibakoudai.jp>
2023-11-24 17:43:46 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -9,16 +9,15 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-  [ "${out}" = 15 ] || ng ${LINENO}
-    　 
-  ### STRANGE INPUT ###
-  out=$(echo あ | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-    　 
-  out=$(echo | ./plus) #空文字
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-    　 
+++ seq 5
+++ ./plus
++ out=15
+[ "${out}" = 15 ] || ng ${LINENO}
++ '[' 15 = 15 ']'
+　
 [ "$res" = 0 ] && echo OK
-  exit $res
++ '[' 0 = 0 ']'
++ echo OK
+OK
+exit $res
++ exit 0
