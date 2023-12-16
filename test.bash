1715ddf2b42fe1c589826e4aa40790409b754927
Ryota Ebisawa <s22C1014KY@s.chibakoudai.jp>
2023-12-16 16:12:00 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -10,14 +10,44 @@ ng () {
 res=0
 
 ### I/O TEST ###
+#plus
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+#subtraction
+out=$(seq 5 | ./subtraction)
+[ "${out}" = -15 ] || ng ${LINENO}
+
+#multiplication
+out=$(seq 5 | ./multiplication)
+[ "${out}" = 120 ] || ng ${LINENO}
+
 ### STRANGE INPUT ###
+#plus
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
+
 out=$(echo | ./plus) #空文字
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}    　
+[ "${out}" = "" ] || ng ${LINENO}
+
+#subtraction
+out=$(echo あ | ./subtraction)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./subtraction) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+#multiplication
+out=$(echo あ | ./multiplication)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./multiplication) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}　
 [ "$res" = 0 ] && echo OK
 exit $res
