346740b9c4f84ca10dccfadd2955658ee42a8baa
EbisawaRyota <147678524+EbisawaRyota@users.noreply.github.com>
2023-12-30 10:50:28 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -46,5 +46,9 @@ out=$(echo あ | ./multiplication)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo | ./multiplication) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
